917220422db17e7a2e8ef93b8af403aaca3cbc43
abl-n <ant@pilgrimsfalk.local>
2023-01-09 21:51:11 +0100

    added /posts path

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def root():
     root page
     """
     return {"message": "welcome to my api!"}
+
+
+@app.get("/posts")
+def get_posts():
+    """
+    Get posts
+    """
+    return {"data": "this is your post"}
